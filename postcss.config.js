dc8aa4cfc100a7759cb7e9c716f04c13c037a04f
Shimomae Hayato <hayato@shimomae.name>
2017-01-26 14:56:50 +0900

    Use ESLint (#18)

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
-module.exports = (ctx) => ({
+module.exports = ctx => ({
   from: ctx.from,
   plugins: {
     'postcss-import': {},
     'postcss-custom-properties': {},
-    cssnano: {}
-  }
+    cssnano: {},
+  },
 });
